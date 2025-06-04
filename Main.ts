07a8582bf77b9f564531e59368a1ac4a35471ae3
LucasAWorkman <lucas.workman@bsu.edu>
2025-06-04 01:08:40 -0400

    added message and interaction creations

--- Main.ts
+++ Main.ts
@@ -41,6 +41,7 @@ const commands = [
 ].map(command => command.toJSON());
 
 
+
 const rest = new REST({ version: '9' }).setToken(config.Token);
 
 (async () => {
@@ -62,6 +63,48 @@ bot.on('ready', ():void => {
     if (bot.user) {
         console.log(`Logged in as ${bot.user.tag}!`);
         bot.user.setActivity('with Discord.js');
+    }
+});
+
+bot.on('interactionCreate', async (interaction): Promise<void> => {
+    if (!interaction.isCommand()) return;
+
+    const command = bot.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(`Error executing command ${interaction.commandName}:`, error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+
+bot.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.type !== 'GUILD_TEXT') return;
+
+    let prefix: string = config.prefix;
+    let messageArgs = message.content.slice(prefix.length).trim().split(/ +/);
+    let cmd = messageargs[0];
+    let args = messageArgs.slice(1);
+
+    if (!message.content.startsWith(prefix)) return;
+
+    let commandName = bot.commands.get(cmd) || bot.commands.find((c) => c.aliases && c.aliases.includes(cmd));
+    let ocmmandfile = bot.commands.get(commandName);
+
+    if (commandfile) {
+        try {
+            await commandfile.execute(message, args);
+        } catch (error) {
+            console.error(`Error executing command ${cmd}:`, error);
+            await message.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
     }
 });
 
